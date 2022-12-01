6dba9068504ec11f6368ef14cab99ef7160da101
paulinho255 <paulinho255@gmail.com>
2022-11-30 23:43:26 -0300

    Ajuste para realizar consultas no banco, retornando apenas uma vez a cada solicitação.

--- src/main/java/br/com/bluesoft/desafiov3/desafiov3/pedido/model/Pedido.java
+++ src/main/java/br/com/bluesoft/desafiov3/desafiov3/pedido/model/Pedido.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
+
 @Entity
 public class Pedido {
 
--- src/main/java/br/com/bluesoft/desafiov3/desafiov3/pedido/repository/PedidoRepository.java
+++ src/main/java/br/com/bluesoft/desafiov3/desafiov3/pedido/repository/PedidoRepository.java
@@ -1,14 +1,15 @@
 package br.com.bluesoft.desafiov3.desafiov3.pedido.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.bluesoft.desafiov3.desafiov3.pedido.model.Pedido;
 
@@ -23,16 +24,16 @@ public class PedidoRepository {
         entityManager.persist(pedido);
         return pedido;
     }
-
     public List<Pedido> listarTodos() {
-        String sql = " from Pedido p";
-
-        final Query query = entityManager.createQuery(sql);
+    	String sql = "SELECT p FROM Pedido p JOIN FETCH p.itens";
+    	
+        final TypedQuery<Pedido> query = entityManager.createQuery(sql, Pedido.class);
         return query.getResultList();
     }
 
     public Pedido buscarPedido(Long pedidoId) {
-        String sql = " from Pedido p where p.id = :pedidoId ";
+    	
+    	String sql = "SELECT p FROM Pedido p JOIN FETCH p.itens WHERE p.id = :pedidoId ";
 
         final Query query = entityManager.createQuery(sql);
 
